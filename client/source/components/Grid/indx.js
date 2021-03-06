33ff19562038cc9d42ec28cfb51408ff7d345d92
Benjamin Shostak <benshostak@rocketmail.com>
2021-03-05 22:19:12 -0800

    finished components

---
+++ client/source/components/Grid/indx.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// Exporting the Container, Row, and Col components 
+
+// This Container component allows us to use a bootstrap container without class names
+export function Container({ fluid, children }) {
+  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+// This Row component lets us use a bootstrap row without class names
+export function Row({ fluid, children }) {
+  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+
+// e.g. <Col size="md-12"> instead of <div className="col-md-12">
+export function Col({ size, children }) {
+  return (
+    <div
+      className={size
+        .split(" ")
+        .map(size => "col-" + size)
+        .join(" ")}
+    >
+      {children}
+    </div>
+  );
+}
